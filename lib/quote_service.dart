c90cd2e8b7eb07a1a10c11ec824768dc273dd437
Talha <itsmuhammadtalha2@gmail.com>
2025-04-18 17:58:32 +0500

    Protected the key

--- lib/quote_service.dart
+++ lib/quote_service.dart
@@ -2,23 +2,18 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'quote_model.dart';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 class QuoteService {
   static Future<Quote> fetchRandomQuote() async {
     try {
-      await dotenv.load(); // Load .env file
-      final apiKey = dotenv.env['API_KEY']; // Fetch API key from the environment
-
       final url = Uri.parse('https://api.api-ninjas.com/v1/quotes');
       final response = await http.get(
         url,
-        headers: {'X-Api-Key': apiKey ?? ''},
+        headers: {'X-Api-Key': 'Enter your key'}
       );
 
       if (response.statusCode == 200) {
         final List<dynamic> json = jsonDecode(response.body);
-        return Quote(text: json[0]['q'], author: json[0]['a']);
+        return Quote(text: json[0]['quote'], author: json[0]['author']);
       } else {
         throw Exception('Failed to load quote: ${response.statusCode}');
       }
